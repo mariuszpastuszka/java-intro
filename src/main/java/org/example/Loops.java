cbfd143e53a107f563877bac91f0f9109d92bfa7
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-02 09:34:02 +0200

    while loop introduced

--- src/main/java/org/example/Loops.java
+++ src/main/java/org/example/Loops.java
@@ -1,7 +1,21 @@
 package org.example;
-
 public class Loops {
     public static void main(String[] args) {
-
+        // repeat that line 100 times
+        System.out.println("Let's play with loops");
+        // while loop is going to be executed as long as condition is true
+        // infinite loop
+//        while (true) {
+//            System.out.println(“Let’s play with loops”);
+//        }
+        int numberOfRepetitions = 10;
+        while (numberOfRepetitions > 0) {
+            System.out.println("Loop is running, number of repetitions is: " + numberOfRepetitions);
+//            numberOfRepetitions = numberOfRepetitions - 1;
+//            numberOfRepetitions-=2; // numberOfRepetitions = numberOfRepetitions - 2;
+            numberOfRepetitions--;
+        }
+        System.out.println("After loop");
+        System.out.println("Number of repetitions is: " + numberOfRepetitions);
     }
 }
