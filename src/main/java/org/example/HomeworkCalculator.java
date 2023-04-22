b603ab3dab4d33aab95f2e71a431e1674fe0f763
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-22 09:00:32 +0200

    adding new function - addManyNumbers

--- src/main/java/org/example/HomeworkCalculator.java
+++ src/main/java/org/example/HomeworkCalculator.java
@@ -2,11 +2,21 @@ package org.example;
 
 public class HomeworkCalculator {
 
-    public int add(int a, int b) {
+    public static void main(String[] args) {
+        System.out.println("Using my homework calculator");
+        add(10, 15);
+        addManyNumbers(1, 1, 2, 2, 1);
+    }
+
+    public static int add(int a, int b) {
         System.out.println("Received parameters, a = " + a + ", b = " + b);
         int result = a + b;
         System.out.println("Calculation result is: " + result);
         return result;
+    }
+
+    public static int addManyNumbers(int a, int b, int c, int d, int e) {
+        return 10;
     }
 
     public int sub(int a, int b) {
