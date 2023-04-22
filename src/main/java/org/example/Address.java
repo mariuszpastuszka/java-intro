aa447ac9aff675df91cbdd791389dd8cbb7c8db3
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-22 11:26:25 +0200

    adding constructors to Address and Phone classes

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -7,4 +7,14 @@ public class Address {
     String street;
     String flatNumber;
     String zipCode;
+
+    public Address() {
+    }
+    public Address(String country, String city, String street, String flatNumber, String zipCode) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.flatNumber = flatNumber;
+        this.zipCode = zipCode;
+    }
 }
--- src/main/java/org/example/Phone.java
+++ src/main/java/org/example/Phone.java
@@ -3,4 +3,12 @@ package org.example;
 public class Phone {
     String countryCode;
     String phoneNumber;
+
+    public Phone(String countryCode, String phoneNumber) {
+        this.countryCode = countryCode;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Phone() {
+    }
 }
