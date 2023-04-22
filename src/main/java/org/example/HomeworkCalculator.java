4c23f4ef991b9c48ffd9dec0c3264744fb6dde8a
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-22 07:54:50 +0200

    Calculator - homework

---
+++ src/main/java/org/example/HomeworkCalculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class HomeworkCalculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+}
