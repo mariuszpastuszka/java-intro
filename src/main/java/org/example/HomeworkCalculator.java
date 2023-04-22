414a5c25065ac697695622e80844f6be0386e890
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-04-22 08:32:10 +0200

    Calculator - homework - refactoring

--- src/main/java/org/example/HomeworkCalculator.java
+++ src/main/java/org/example/HomeworkCalculator.java
@@ -3,7 +3,10 @@ package org.example;
 public class HomeworkCalculator {
 
     public int add(int a, int b) {
-        return a + b;
+        System.out.println("Received parameters, a = " + a + ", b = " + b);
+        int result = a + b;
+        System.out.println("Calculation result is: " + result);
+        return result;
     }
 
     public int sub(int a, int b) {
